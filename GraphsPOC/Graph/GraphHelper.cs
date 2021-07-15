daca8c119a64d56881b602b86a80e7bc925f8bd8
Seth Haugland <shauglan_stu@kent.edu>
2021-07-15 11:37:37 -0400

    Add project files.

---
+++ GraphsPOC/Graph/GraphHelper.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimeZoneConverter;
+
+namespace GraphTutorial
+{
+    public class GraphHelper
+    {
+        private static GraphServiceClient graphClient;
+        public static void Initialize(IAuthenticationProvider authProvider)
+        {
+            graphClient = new GraphServiceClient(authProvider);
+        }
+
+        public static async Task<User> GetMeAsync()
+        {
+            try
+            {
+                // GET /me
+                return await graphClient.Me
+                    .Request()
+                    .Select(u => new {
+                        u.DisplayName,
+                        u.MailboxSettings
+                    })
+                    .GetAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<Event>> GetCurrentWeekCalendarViewAsync(
+    DateTime today,
+    string timeZone)
+        {
+            // Configure a calendar view for the current week
+            var startOfWeek = GetUtcStartOfWeekInTimeZone(today, timeZone);
+            var endOfWeek = startOfWeek.AddDays(7);
+
+            var viewOptions = new List<QueryOption>
+    {
+        new QueryOption("startDateTime", startOfWeek.ToString("o")),
+        new QueryOption("endDateTime", endOfWeek.ToString("o"))
+    };
+
+            try
+            {
+                var events = await graphClient.Me
+                    .CalendarView
+                    .Request(viewOptions)
+                    // Send user time zone in request so date/time in
+                    // response will be in preferred time zone
+                    .Header("Prefer", $"outlook.timezone=\"{timeZone}\"")
+                    // Get max 50 per request
+                    .Top(50)
+                    // Only return fields app will use
+                    .Select(e => new
+                    {
+                        e.Subject,
+                        e.Organizer,
+                        e.Start,
+                        e.End
+                    })
+                    // Order results chronologically
+                    .OrderBy("start/dateTime")
+                    .GetAsync();
+
+                return events.CurrentPage;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting events: {ex.Message}");
+                return null;
+            }
+        }
+
+        private static DateTime GetUtcStartOfWeekInTimeZone(DateTime today, string timeZoneId)
+        {
+            // Time zone returned by Graph could be Windows or IANA style
+            // .NET Core's FindSystemTimeZoneById needs IANA on Linux/MacOS,
+            // and needs Windows style on Windows.
+            // TimeZoneConverter can handle this for us
+            TimeZoneInfo userTimeZone = TZConvert.GetTimeZoneInfo(timeZoneId);
+
+            // Assumes Sunday as first day of week
+            int diff = System.DayOfWeek.Sunday - today.DayOfWeek;
+
+            // create date as unspecified kind
+            var unspecifiedStart = DateTime.SpecifyKind(today.AddDays(diff), DateTimeKind.Unspecified);
+
+            // convert to UTC
+            return TimeZoneInfo.ConvertTimeToUtc(unspecifiedStart, userTimeZone);
+        }
+    }
+}
---
+++ GraphsPOC/Program.cs
@@ -0,0 +1,115 @@
+﻿using GraphsPOC.Authentication;
+using GraphTutorial;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+
+namespace GraphsPOC
+{
+    class Program
+    {
+        static IConfigurationRoot LoadAppSettings()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            // Check for required settings
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                string.IsNullOrEmpty(appConfig["scopes"]))
+            {
+                return null;
+            }
+
+
+            return appConfig;
+        }
+
+        static string FormatDateTimeTimeZone(
+            Microsoft.Graph.DateTimeTimeZone value,
+            string dateTimeFormat)
+        {
+            // Parse the date/time string from Graph into a DateTime
+            var dateTime = DateTime.Parse(value.DateTime);
+
+            return dateTime.ToString(dateTimeFormat);
+        }
+
+        static void Main(string[] args)
+        {
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var scopes = scopesString.Split(';');
+
+            // Initialize the auth provider with values from appsettings.json
+            var authProvider = new DeviceCodeAuthProvider(appId, scopes);
+
+            // Request a token to sign in the user
+            var accessToken = authProvider.GetAccessToken().Result;
+
+            Console.WriteLine($"Access token: {accessToken}\n");
+
+            // Initialize Graph client
+            GraphHelper.Initialize(authProvider);
+
+            // Get signed in user
+            var user = GraphHelper.GetMeAsync().Result;
+            Console.WriteLine($"Welcome {user.DisplayName}!\n");
+
+            GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+            var @event = new Event
+            {
+                Subject = "Let's go for lunch",
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Html,
+                    Content = "Does next month work for you?"
+                },
+                Start = new DateTimeTimeZone
+                {
+                    DateTime = "2021-07-15T12:00:00",
+                    TimeZone = "Pacific Standard Time"
+                },
+                End = new DateTimeTimeZone
+                {
+                    DateTime = "2021-07-15T14:00:00",
+                    TimeZone = "Pacific Standard Time"
+                },
+                Location = new Location
+                {
+                    DisplayName = "Test Location"
+                },
+                Attendees = new List<Attendee>()
+                {
+                    new Attendee
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "shauglan_stu@kent.edu",
+                            Name = "Seth Haugland"
+                        },
+                        Type = AttendeeType.Required
+                    }
+                },
+                    IsOnlineMeeting = true,
+                    OnlineMeetingProvider = OnlineMeetingProviderType.TeamsForBusiness
+            };
+
+            var something = graphClient.Me.Calendar.Events
+                .Request()
+                .AddAsync(@event).Result;
+
+            Console.WriteLine("Event should now be scheduled");
+        }
+    }
+}
